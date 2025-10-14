f74b6bbb3f89f85c3b405d62bd5e4d542323fadb
laluska <llagator7@gmail.com>
2025-10-14 20:40:27 +0200

    Create zad2.cpp

---
+++ vj2/zad2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+struct vektor {
+	int lv;
+	int fv;
+	int *niz;
+};
+struct vektor  vector_new(int n) {
+	int *niz = new int[n];
+	vektor.fv = 0;
+
+}
+int main() {
+	int n;
+	return 0;
+}
