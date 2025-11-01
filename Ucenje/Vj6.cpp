8319f2c925326f964d2dfa2de33a6735b8a37781
laluska <llagator7@gmail.com>
2025-11-01 13:40:36 +0100

    Create Vj6.cpp

---
+++ Ucenje/Vj6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+// Napišite funkciju koja za string koji predstavlja ime i prezime vraća string u kojem su
+//inicijali.
+//"John Johnny Depp"-> "J.J.D."
+string funkcija(const string& s) {
+    string inicijali;
+
+    for (char c : s) {
+        if (isupper(c)) {
+            inicijali.push_back(c);
+            inicijali.push_back('.'); // dodaj tačku iza svakog inicijala
+        }
+    }
+
+    return inicijali;
+}
+//mogu li po velikom slovu ili moram po razmaku???
+int main() {
+    string s = "Laura Lagator";
+    cout<<"inicijali su"<<funkcija(s);
+    return 0;
+}
