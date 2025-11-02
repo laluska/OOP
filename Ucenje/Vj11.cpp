4d2b12bbe445cfc3d2ec067bfec2f9920b8210b5
laluska <llagator7@gmail.com>
2025-11-02 17:28:00 +0100

    Update Vj11.cpp

--- Ucenje/Vj11.cpp
+++ Ucenje/Vj11.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*  Napišite funkciju koja u rečenici iza svakog samoglasnika doda znak ”:”.*/
+void funkcija(string& s){
+    auto it=s.begin();
+    while(it!=s.end()){
+       it= find_if(it, s.end(), [](char c){
+            return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+                   c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+        });
+        if(it==s.end()){break;}
+        else{
+            it=s.insert(it+1, ':');
+            ++it;
+        }
+        ++it;
+    }
+    cout<<"string je"<<s<<endl;
+    
+}
+int main(){
+    string s="Pas je mal";
+    funkcija(s);
+    return 0;
+}
